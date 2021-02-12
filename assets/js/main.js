cebe43af1cf1939402fb6a8c7b2d981f1f882ad6
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-02-12 10:26:58 +0100

    fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,23 +16,31 @@ function changeKlasse(){
     betrag.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro<span>*</span>"
     resultParagraph.innerHTML = "Bruttobetrag (Endpreis)"
     if(prozent19){
-      mehrwertsteuerBetrag19 = zahl.value *19/100;
-      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag19
-      nettoBruttoBetrag.innerHTML = zahl.value*1+ mehrwertsteuerBetrag19*1
+      mehrwertsteuerBetrag19 = zahl.value*19/100;
+      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag19.toFixed(2)
+      summe19 = zahl.value*1+ mehrwertsteuerBetrag19*1
+      nettoBruttoBetrag.innerHTML = summe19.toFixed(2)
     }
     else{
       mehrwertsteuerBetrag7 = zahl.value *7/100;
-      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag7
-      nettoBruttoBetrag.innerHTML = zahl.value*1+ mehrwertsteuerBetrag7*1
+      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag7.toFixed(2)
+      summe7 = zahl.value*1+ mehrwertsteuerBetrag7*1
+      nettoBruttoBetrag.innerHTML = summe7.toFixed(2)
     }
   } else{
     betrag.innerHTML = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro"
     resultParagraph.innerHTML = "Nettobetrag"
     if(prozent19){
-      mehrwertsteuer.innerHTML = zahl.value *19/100;
+      nettoBetrag19 = zahl.value/1.19
+      nettoBruttoBetrag.innerHTML = nettoBetrag19.toFixed(2)
+      mehrwertsteuerBetrag19_2 = zahl.value-nettoBetrag19
+      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag19_2.toFixed(2)
     }
     else{
-      mehrwertsteuer.innerHTML = zahl.value *7/100;
+      nettoBetrag7 = zahl.value/1.07
+      nettoBruttoBetrag.innerHTML = nettoBetrag7.toFixed(2)
+      mehrwertsteuerBetrag7_2 = zahl.value-nettoBetrag7
+      mehrwertsteuer.innerHTML = mehrwertsteuerBetrag7_2.toFixed(2)
     }
   }
 
